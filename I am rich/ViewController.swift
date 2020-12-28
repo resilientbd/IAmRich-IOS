35faa6abf578757c56564e6569f3260885843619
sk. faisal <faisal.hossain.pk@gmail.com>
2020-12-28 11:36:45 +0600

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Sk Faisal on 28/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
